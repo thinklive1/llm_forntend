114d0c9ff78910f961e6ed8b7f13d6ccaf79bbef
thinklive1 <469631989@qq.com>
2025-07-30 10:54:45 +0800

    test new ps1 2025-07-30 10:54:37

--- auto_git.ps1
+++ auto_git.ps1
@@ -6,7 +6,8 @@ Write-Host "已提交更改: $message"
 $confirmation = Read-Host "是否推送到远处仓库？ (y/n)"
 if ($confirmation -eq 'y' -or $confirmation -eq 'Y') {
     Write-Host "推送至远程仓库"
-    git push -f origin main
+    git push origin main
+    git push github_origin main
 } else {
     Write-Host "推送取消"
 }
